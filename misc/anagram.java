355bafaab255da92fc989d15fd47c29749afffc6
VinitraMk <vinitramk@gmail.com>
2018-07-05 01:12:03 +0530

    updated logic anagram

--- misc/anagram.java
+++ misc/anagram.java
@@ -7,48 +7,38 @@ class anagram{
         int n=sc.nextInt();
         int i,j,k;
         String[] a = new String[n];
-        int mat[][] = new int[n][26];
-        for(i=0;i<n;i++) {
-            for(j=0;j<26;j++)
-                mat[i][j]=0;
-        }
 
-        for(i=0;i<n;i++)
-            a[i]=sc.next();
         for(i=0;i<n;i++) {
-            for(j=0;j<a[i].length();j++) 
-                mat[i][a[i].charAt(j)-'a']++;
+            a[i]=sc.next();
         }
 
         int c=0;
-        Arrays.sort(a,0,n);
-        int dp[] = new int[n];
-        Arrays.fill(dp,0,n,0);
 
-        for(i=1;i<n;i++) {
-            if(a[i].length()!=a[i-1].length())
-                dp[i]=0;
-            else {
-                int co[]=new int[26];
-                Arrays.fill(co,0,26,0);
-                int m=a[i].length();
-                int f=0;
-                for(j=0;j<m;j++) {
-                    co[a[i-1].charAt(j)-'a']++;
-                    co[a[i].charAt(j)-'a']--;
-                }
-                for(j=0;j<26;j++) {
-                    if(co[j]!=0) {
-                        f=1;
-                        break;
-                    }
-                }
-                if(f==0) {
-                    System.out.println(a[i]+" and "+a[i-1]+" are anagrams.");
-                    dp[i]=dp[i-1]+1;
-                }
-            }
+        HashMap<Integer,Integer> hp = new HashMap<Integer,Integer>();
+        for(i=0;i<n;i++) {
+            int hs=hash(a[i]);
+            //System.out.println(a[i]+" "+hs+" "+hp.get(hs));
+            if(hp.containsKey(hs)) 
+                hp.put(hs,hp.get(hs)+1);
+            else
+                hp.put(hs,1);
         }
-	}
+        for(int in:hp.keySet())
+            c+=hp.get(in);
+        System.out.println(c);
+    }
+
+    public static int hash(String s) {
+        int freq[] = new int[26];
+        for(int i=0;i<s.length();i++) {
+            freq[s.charAt(i)-'a']++;
+        }
+        int t=1,ret=0;
+        for(int i=0;i<26;i++) {
+            ret+=(t*freq[i]);
+            t=t*1000003;
+        }
+        return ret;
+    }
 }
 
